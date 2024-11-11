4c41d4aceb89d7ab8392da40278921abfc58b417
enriqueteru <info@enriqueteruel.com>
2024-11-11 02:54:07 +0100

    feat(next): test enriqueteruel.com 3

--- enriqueteruel/src/app/page.tsx
+++ enriqueteruel/src/app/page.tsx
@@ -1,9 +1,12 @@
-"use client"; 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold text-center">Enrique Teruel</h1>
-      <p className="text-center">En construcción</p>
+      <main className="flex flex-col gap-2 row-start-2 items-center sm:items-start">
+        <h3 className="text-3xl font-semibold"> ¡Hola! Soy Enrique Teruel</h3>
+        <p> Soy ingeniero de software y actualmente trabajo en @INDRA</p>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      </footer>
     </div>
   );
 }
